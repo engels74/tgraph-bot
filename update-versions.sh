7976c555a617f49ba0c703970189d6b7e505c015
nboxdev <141435164+engels74@users.noreply.github.com>
2024-06-16 17:47:03 +0200

    test build

--- update-versions.sh
+++ update-versions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/StuffAnThings/qbit_manage/releases/latest" | jq -re .tag_name) || exit 1
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/engels74/tgraph-bot-source/releases/latest" | jq -re .tag_name) || exit 1
 [[ -z ${version} ]] && exit 0
 [[ ${version} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
-    --arg version "${version//v/}" \
+    --arg version "${version}" \
     '.version = $version' <<< "${json}" | tee VERSION.json
